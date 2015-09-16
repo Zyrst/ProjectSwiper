6fabc04cd2838dbc02ce723b2b3d43fecb36a0cb
Zyrst <zajreg@gmail.com>
2015-09-16 15:29:19 +0200

    Character base class

---
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Character {
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (GetComponent<ClickAttack>().getNewHit())
+        {
+
+        }
+	}
+
+    public override void Die()
+    {
+        base.Die();
+    }
+}
