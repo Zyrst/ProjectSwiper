bb19fa3603e545aaabbbdef855f69d2820b4290d
Jeflux <jespermathiason@gmail.com>
2015-09-16 16:32:20 +0200

    Mer hälsa

--- Assets/Scripts/GUI/Healthbar.cs
+++ Assets/Scripts/GUI/Healthbar.cs
@@ -3,27 +3,31 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Linq;
 
-[ExecuteInEditMode]
 public class Healthbar : MonoBehaviour {
 
-    public GameObject enemy;
+    public Enemy enemy;
     public Image _foreground;
-    
+    public Image _background;
+    public Image _border;
+
     // Use this for initialization
 	void Start () {
-        if (_foreground == null)
-            _foreground = GetComponentsInChildren<Image>().FirstOrDefault(x => x.name == "Foreground");
-        Debug.Assert(_foreground != null, "Could not find reference to image");
+        //if (_foreground == null)
+        //    _foreground = GetComponentsInChildren<Image>().FirstOrDefault(x => x.name == "Foreground");
+        Debug.Assert(_foreground != null, "Could not find reference to foreground");
+        Debug.Assert(_background != null, "Could not find reference to background");
+        Debug.Assert(_border     != null, "Could not find reference to border");
 
         // Try to find enemy
         if (transform.parent != null)
-            enemy = transform.parent.gameObject;
+            enemy = transform.parent.GetComponent<Enemy>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(Camera.main.transform);
-
-        // TODO: Get enemy health and scale accordingly
+        
+        if (enemy != null)
+            _foreground.transform.localScale = new Vector3(enemy._maxHealth / enemy._health, 1, 1);
     }
 }
