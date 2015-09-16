b68809b081264ef7b03a738c9f1744f3b7a83be5
Zyrst <zajreg@gmail.com>
2015-09-16 15:38:26 +0200

    Savemanager start

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class SaveManager {
+
+    [System.Serializable]
+    public class GameInfo
+    {
+        //Put things in here later
+        
+    }
+
+    public static GameInfo _info = new GameInfo();
+    public static void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGame.dfq");
+        bf.Serialize(file, new GameInfo());
+        file.Close();
+    }
+
+    public static void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/savedGame.dfq"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGame.dfq", FileMode.Open);
+            _info = (GameInfo)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+    
+}
