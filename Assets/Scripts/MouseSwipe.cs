088e0f6fdc0061da3f0022658d5e4605221905bd
smedisen <smedberg_5@hotmail.com>
2015-09-22 08:40:50 +0200

    fin swipe

--- Assets/Scripts/MouseSwipe.cs
+++ Assets/Scripts/MouseSwipe.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class MouseSwipe : MonoBehaviour {
 
     public LineRenderer _lineRender;
-    [HideInInspector]
     public int _count = 2;
-    [HideInInspector]
+
     private Vector3[] vertecies = null;
+    [HideInInspector]
+    private bool firstFrameElse = true;
 
 	// Use this for initialization
 	void Start () {
@@ -17,18 +18,44 @@ public class MouseSwipe : MonoBehaviour {
         vertecies = new Vector3[_count];
         _lineRender.SetVertexCount(_count);
         _lineRender.SetWidth(0.5f, 0.01f);
+
+        firstFrameElse = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        vertecies[0] = MouseController.Instance.worldPosition;
-        _lineRender.SetPosition(0, vertecies[0]);
+        if (MouseController.Instance.buttonDown)
+        {
+            if (!firstFrameElse)
+            {
+                for (int i = 0; i < _count; i++)
+                {
+                    vertecies[i] = MouseController.Instance.worldPosition + (Camera.main.transform.forward * 5);
+                    _lineRender.SetPosition(i, vertecies[i]);
+                }
+            }
+            firstFrameElse = true;
 
-        for (int i = _count-1; i > 0; i--)
-        {   
-            vertecies[i] = vertecies[i-1];
-            _lineRender.SetPosition(i, vertecies[i]);
+            vertecies[0] = MouseController.Instance.worldPosition + (Camera.main.transform.forward * 5);
+            _lineRender.SetPosition(0, vertecies[0]);
+
+            for (int i = _count - 1; i > 0; i--)
+            {
+                vertecies[i] = vertecies[i - 1];
+                _lineRender.SetPosition(i, vertecies[i]);
+            }
         }
-
+        else
+        {
+            if (firstFrameElse)
+            {
+                firstFrameElse = false;
+                for (int i = 0; i < _count; i++)
+                {
+                    vertecies[i] = new Vector3(-1, -1, -1);
+                    _lineRender.SetPosition(i, vertecies[i]);
+                }
+            }
+        }
 	}
 }
