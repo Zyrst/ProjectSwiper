2b5b820238184a2f7e035a1c98efe17313ad7e21
TheAtomicGnome <mattias94@live.se>
2015-09-22 09:07:55 +0200

    Fan Jesper

--- Assets/Scripts/Characters/Enemies/Enemy.cs
+++ Assets/Scripts/Characters/Enemies/Enemy.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Enemy : Character {
 
+    public float _attackCooldown = 1;
+    float _attackCounter = 0;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,7 +13,7 @@ public class Enemy : Character {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        UpdateAttackCooldown();
 	}
 
     public override void Die()
@@ -20,5 +23,21 @@ public class Enemy : Character {
         Debug.Log(gameObject.GetInstanceID() + " avled tragiskt");
 
         Destroy(gameObject);
+    }
+
+    void UpdateAttackCooldown()
+    {
+        _attackCounter += 1;
+
+        if (_attackCounter >= _attackCooldown)
+        {
+            _attackCounter = 0;
+            AttackPlayer();
+        }
+    }
+
+    void AttackPlayer()
+    {
+        Debug.Log(gameObject.GetInstanceID() + " attakerade spelaren");
     }
 }
