c767197a9abe4ecee41594b801875ad8b9359559
TheAtomicGnome <mattias94@live.se>
2015-09-22 14:27:39 +0200

    Git kan fan inte merga

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -32,18 +32,12 @@ public class Character : MonoBehaviour {
 
     public void Damage(float damage_)
     {
-<<<<<<< HEAD
-=======
-       // Debug.Log(_health);
-
->>>>>>> 38ac4a1d78d1886fa5802d3a20f594f9d445c109
         _health -= Mathf.Abs(damage_);
 
         if (_health <= 0)
         {
             Die();
         }
-<<<<<<< HEAD
     }
 
     public void Heal(float health_)
@@ -63,25 +57,6 @@ public class Character : MonoBehaviour {
 
     public virtual void Die()
     {
-
-    }
-}
-=======
-    }
-
-    public void Heal(float health_)
-    {
-        _health += Mathf.Abs(health_);
-
-        if (_health > _maxHealth)
-        {
-            _health = _maxHealth;
-        }
-    }
-
-    public virtual void Die()
-    {
         _isDead = true;
     }
 }
->>>>>>> 38ac4a1d78d1886fa5802d3a20f594f9d445c109
