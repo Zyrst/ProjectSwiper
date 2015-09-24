b02308335ffdd1699550b8166f79a42d54c1bc54
TheAtomicGnome <mattias94@live.se>
2015-09-24 16:42:12 +0200

    Glömde hur git bash fungerar

---
+++ Assets/Scripts/GUI/LevelGUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelGUI : Text {
+
+    string _string = "Level: ";
+
+    protected override void Start()
+    {
+        base.Start();
+        _string = text;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.text = _string + Game.Instance._level;
+	}
+}
---
+++ Assets/Scripts/GUI/UpgradeButtonGUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpgradeButtonGUI : Button {
+
+    public override void OnPointerClick(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        upgradePlayer();
+    }
+
+    private void upgradePlayer()
+    {
+        if (Game.Instance._gameCurrency >= 100)
+        {
+            Game.Instance._currentPlayer.GetComponent<Player>()._damage += 1;
+            Game.Instance._currentPlayer.GetComponent<Player>()._maxHealth += 10;
+            Game.Instance._gameCurrency -= 100;
+            Game.Instance._level += 1;
+            SaveManager.Save();
+            Debug.Log("Upgraded player");
+        }
+        else
+        {
+            Debug.Log("Not enough currency");
+        }
+    }
+}
