bfc389f64d5e39164e2e5f4f03d556ac57dfc5bc
TheAtomicGnome <mattias94@live.se>
2015-09-24 09:39:09 +0200

    Stadade lite

--- Assets/Scripts/Characters/Enemies/Enemy.cs
+++ Assets/Scripts/Characters/Enemies/Enemy.cs
@@ -4,13 +4,7 @@ using System.Collections;
 public class Enemy : Character {
 	// Use this for initialization
 	void Start () {
-	    GameObject healthBar = Instantiate(UnityEngine.Resources.Load("Prefabs/Combat/Healthbar")) as GameObject;
-        healthBar.transform.SetParent(transform);
-        healthBar.GetComponent<RectTransform>().localPosition = new Vector3(0, 1, 0);
-
-        GameObject cooldownBar = Instantiate(UnityEngine.Resources.Load("Prefabs/Combat/Cooldownbar")) as GameObject;
-        cooldownBar.transform.SetParent(transform);
-        cooldownBar.GetComponent<RectTransform>().localPosition = new Vector3(0, -1, 0);
+        InitializeGUI();
     }
 	
 	// Update is called once per frame
@@ -18,14 +12,30 @@ public class Enemy : Character {
         
     }
 
+    void InitializeGUI()
+    {
+        GameObject healthBar = Instantiate(UnityEngine.Resources.Load("Prefabs/Combat/Healthbar")) as GameObject;
+        healthBar.transform.SetParent(transform);
+        healthBar.GetComponent<RectTransform>().localPosition = new Vector3(0, 1, 0);
+
+        GameObject cooldownBar = Instantiate(UnityEngine.Resources.Load("Prefabs/Combat/Cooldownbar")) as GameObject;
+        cooldownBar.transform.SetParent(transform);
+        cooldownBar.GetComponent<RectTransform>().localPosition = new Vector3(0, -1, 0);
+    }
+
     public override void Die()
     {
         base.Die();
 
-        Debug.Log(gameObject.GetInstanceID() + " avled tragiskt");
+        //Debug.Log(gameObject.GetInstanceID() + " avled tragiskt");
 
-        Vector3 dir = (Vector3.up * 15f) + -(transform.position - Camera.main.transform.position);
-        CurrencyObject.Spawn(dir, transform.position + new Vector3(0,1,0));
+        SpawnCurrency();
         Destroy(gameObject);
+    }
+
+    void SpawnCurrency()
+    {
+        Vector3 dir = (Vector3.up * 15f) + -(transform.position - Camera.main.transform.position);
+        CurrencyObject.Spawn(dir, transform.position + new Vector3(0, 1, 0));
     }
 }
