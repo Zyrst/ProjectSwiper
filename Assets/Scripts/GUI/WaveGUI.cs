218ef579cbf4941e3592191adb8d6a76e11afba9
Jeflux <jespermathiason@gmail.com>
2015-10-06 16:21:37 +0200

    Wave gui

---
+++ Assets/Scripts/GUI/WaveGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveGUI : Text {
+
+    public string _string = "Wave: ";
+
+    protected override void Start()
+    {
+        base.Start();
+        _string = text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.text = _string + Game.Instance._combat.GetComponent<Combat>()._waveCounter;
+    }
+}
