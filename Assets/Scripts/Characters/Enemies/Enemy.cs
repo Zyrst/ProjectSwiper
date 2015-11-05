760a04425267dbc7396242f744f703b536093f5b
Zyrst <zajreg@gmail.com>
2015-11-05 09:33:16 +0100

    Removed game analytic plugin, player health fix

--- Assets/Scripts/Characters/Enemies/Enemy.cs
+++ Assets/Scripts/Characters/Enemies/Enemy.cs
@@ -54,6 +54,6 @@ public class Enemy : Character {
     void SpawnCurrency()
     {
         Vector3 dir = (Vector3.up * 15f) + -(transform.position - Camera.main.transform.position);
-        CurrencyObject.Spawn(dir, transform.position + new Vector3(-1.5f, 1f, -1.5f));
+        CurrencyObject.Spawn(dir * 5, transform.position + new Vector3(-1.5f, 1f, -1.5f));
     }
 }
