b07d3bae538daaad263b446f2b3dc40e600e2347
Jeflux <jespermathiason@gmail.com>
2015-11-19 10:43:12 +0100

    UI stöf

--- Assets/Scripts/GUI/PlayerHealthGUI.cs
+++ Assets/Scripts/GUI/PlayerHealthGUI.cs
@@ -5,15 +5,19 @@ using System.Linq;
 
 public class PlayerHealthGUI : MonoBehaviour {
     GameObject life;
-	// Use this for initialization
+    Vector2 _originalSize;
+    
+    // Use this for initialization
 	void Start () {
        life =  GetComponentsInChildren<Image>().FirstOrDefault(x => x.name == "Life").gameObject;
-	}
+       _originalSize = life.GetComponent<RectTransform>().sizeDelta;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         float perc = References.Instance._currentPlayer._health / References.Instance._currentPlayer._maxHealth;
-        life.transform.localScale = new Vector3(perc, 1f, 1f);
-	}
+        //life.transform.localScale = new Vector3(perc, 1f, 1f);
+        life.GetComponent<RectTransform>().sizeDelta = new Vector2(_originalSize.x * perc , _originalSize.y);
+    }
 
 }
